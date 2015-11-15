0a3c99002c12bf2aad6f9d253cf4e52a865bcded
snehaltiwari10 <snehaltiwari10@gmail.com>
2015-11-15 15:21:31 +0530

    Create game.h

---
+++ game.h
@@ -0,0 +1,10 @@
+#define _GAME_H
+int InitialiseGame(state *ptr);
+int ReinitialiseGame(state *ptr);
+void RepopulateBoard(state *ptr);
+void SetMap(state *ptr, int , int , char );
+int GetMap(state *ptr, int , int , char *);
+int IsOpenArea(char );
+int IsOpenPos(state *ptr, int , int );
+tGameEnd GameLoop(void *pCtx, state *ptr);
+void MainGame(void *pCtx, state *ptr);
